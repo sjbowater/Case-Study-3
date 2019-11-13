e6841685c46a2f1f57be4b5cdf73d807c5893fae
peyton-gozon <peyton.gozon@wustl.edu>
2019-11-13 16:56:27 -0600

    Added base files.

---
+++ RCfilter.m
@@ -0,0 +1,15 @@
+% function cascadedRCfilter(Vin,h) receives a time-series voltage sequence
+% sampled with interval h, and returns the output voltage sequence produced
+% by a circuit
+%
+% inputs:
+% Vin - time-series vector representing the voltage input to a circuit
+% h - scalar representing the sampling interval of the time series in
+% seconds
+%
+% outputs:
+% Vout - time-series vector representing the output voltage of a circuit
+
+function Vout = RCfilter(Vin,h)
+Vout = Vin;
+end
