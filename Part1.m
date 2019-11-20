a2cea05999aa96ebff694be54a201bb32daef822
peyton-gozon <peyton.gozon@wustl.edu>
2019-11-20 17:06:57 -0600

    Touched up the variable names, declarations, and qawsedrftgyhujikolijuhgvfcdxs.

--- Part1.m
+++ Part1.m
@@ -7,9 +7,6 @@ close all;
 
 t = 5; % total time of 5 seconds, as seen in figure 2 of the handout. 
 
-%  (8) v_(R,k) = V_(in,k) - V_(C,k)
-% (10) V_(C_k+1) = (1-h/RC)v_(C,k) + h/RC * v_(in,k) 
-
 R = 1; % 1000; % 1 k ohms
 C = 1; % 1e-6; % 1 micro F
 h = 8e-4; % sampling rate in seconds per sample
@@ -28,7 +25,7 @@ V = zeros(3, t/h); % t/h is the total number of indicies 'k'
 V(3,:) = 5; % V_in is a constant 5V the entire time. 
 
 for k = 1:t/h
-    V(r, k) = V(in, k) - V(c, k); % Equation #8
+    V(r, k) = V(in, k) - V(c, k);                                         % Equation #8
     V(c, k+1) = (1 - (h / (R * C))) * V(c, k) + (h / (R * C)) * V(in, k); % Equation #10
 end
 
@@ -39,8 +36,8 @@ hold on;
 plot(V(c, :));
 plot(V(in, :));
 hold off;
-set(gca, 'XTick', 0:6250/5:k)
-set(gca, 'XTickLabel', 0:5)
+set(gca, 'XTick', 0:k/t:k)
+set(gca, 'XTickLabel', 0:t)
 xlabel("Time (s)");
 ylabel("Voltage (V)");
 title("Approximated Charge Curve vs Time");
@@ -61,7 +58,7 @@ xlabel("Time (s)");
 ylabel("Voltage (V)");
 title("Theoretical Charge Curve vs Time");
 legend("V_c", "V_in", "location", "best");
-xlim([0 5]);
+xlim([0 t]);
 ylim([0 5]);
 
 % A comparison of the charges:
