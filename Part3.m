1cf75a4bbba8bb699417c2a2fab56f4931bb35e9
sjbowater <sjbowater@wustl.edu>
2019-11-24 20:46:26 -0600

    ignore

--- Part3.m
+++ Part3.m
@@ -3,52 +3,16 @@
 clear all;
 close all;
 
-<<<<<<< HEAD
-%% 
-t = 0.01;    % Total time simulation runs for. 
-h = 8e-6;    % Sample rate in seconds per second
-C1 = 0.68e-6;
-C3=0.68e-6;% C_1 = C_2 = C_3 = 0.68 microF. 
-R4=330;
-R2=330;    % R_1 = R_2 = R_4 = 330 Ohms
-=======
 %% Circuit C
 t = 0.01;    % Total time simulation runs for, as seen in Figure 7. It's worth noting that if t = 0.001, the simulation appears better, but still wholly inacurate. 
 h = 8e-6;    % Sample rate in seconds per second; this is the same rate from Part 1.
 C = 0.68e-6; % C_1 = C_2 = C_3 = 0.68 microF. 
 R = 330;     % R_1 = R_2 = R_4 = 330 Ohms
->>>>>>> 89be355a48c74f6a330f6208564daaa7cbebea9c
 f1 = 440;    % 440 Hz
 f2 = 3000;   % 3k Hz
 
 timesteps = 0:h:t;
 
-<<<<<<< HEAD
-% A = [ 1 -1 -1  0  0  0; % i_1 - i_2 - i_3 = 0     Equation 15
-%       0  0  0  1  0  0; % V_in = V_in,k           Equation 19
-%       0  0  0  1 -1  0; % V_in - V_1 = V_C1,k     Equation 20
-%       0  0  0  0  1 -1; % V_1 - V_out = V_c3,k    Equation 21
-%       0  R  0  0 -1  0; % R_2 * i_2 - V_1 = 0     Equation 17
-%       0  0  R  0  0 -1; % R_4 * i_3 - V_out = 0   Equation 18
-%     ];
-A = [ 1 -1 -1  0  0  0;
-     -1  1  1  0  0  0;
-      0  0  0  1 -1  0;
-      0  0  0  0 -1  1;
-      0  0 -R4 0  0  1;
-      0 -R2 0  0  1  0;
-      0  0  0  1  0  0;
-              ];
-
-% % same as the A above but with equations in order. 
-% A = [ 1 -1 -1  0  0  0;
-%       0 -R  0  0  1  0;
-%       0  0 -R  0  0  1;
-%       0  0  0  1  0  0;
-%       0  0  0  1 -1  0;
-%       0  0  0  0  1 -1;
-%      ];
-=======
 AC = [ 1 -1 -1  0  0  0; % i_1 - i_2 - i_3 = 0     Equation 15
        0  0  0  1  0  0; % V_in = V_in,k           Equation 19
        0  0  0  1 -1  0; % V_in - V_1 = V_c1,k     Equation 20
@@ -56,7 +20,6 @@ AC = [ 1 -1 -1  0  0  0; % i_1 - i_2 - i_3 = 0     Equation 15
        0  R  0  0 -1  0; % R_2 * i_2 - V_1 = 0     Equation 17
        0  0  R  0  0 -1; % R_4 * i_3 - V_out = 0   Equation 18
      ];
->>>>>>> 89be355a48c74f6a330f6208564daaa7cbebea9c
 
 % The voltages we're tracking. 
 Vc1  = zeros(1, length(timesteps));
@@ -69,21 +32,6 @@ Vin  = 5 * sin(2 * pi * f1 * timesteps) + sin(2 * pi * f2 * timesteps); % Equati
 i1 = zeros(1, length(timesteps));
 i3 = zeros(1, length(timesteps));
 
-<<<<<<< HEAD
-for k=1:length(timesteps)
-    
-    x = linsolve(A, [0, 0, Vc1(k), -Vc3(k), 0, 0, Vin(k),]');
-%     x = linsolve(A, [0 0 0 Vin(k) Vc1(k) Vout(k)]');
-    i1(k)   = x(1);
-    i2(k)   = x(2);
-    i3(k)   = x(3);
-    Vin(k)  = x(4);
-    V1(k)  = x(5);
-    Vout(k) = x(6);
-    
-    Vc1(k+1) = Vc1(k) + (h / C1) * i1(k); % Equation 24
-    Vc3(k+1) = Vc3(k) + (h / C3) * i3(k); % Equation 25
-=======
 for k = 1:length(timesteps)
     % Solve the circuit. 
     x = linsolve(AC, [0, Vin(k), Vc1(k), Vc3(k), 0, 0]');
@@ -150,7 +98,6 @@ for k = 1:length(timesteps)
     % Update Equations
     Vc2(k+1) = Vc2(k) + (h / C) * i2(k); % Equation 24
     Vc3(k+1) = Vc3(k) + (h / C) * i3(k); % Equation 25
->>>>>>> 89be355a48c74f6a330f6208564daaa7cbebea9c
 end
 
 figure;
@@ -162,11 +109,7 @@ legend("Vin", "Vout", "location", "best");
 
 xlabel("Time (s)");
 ylabel("Voltage (V)");
-<<<<<<< HEAD
-title("Voltage (V) vs Time (s) Circuit C");
-=======
-title("Circuit D: Voltage (V) vs Time (s)");
->>>>>>> 89be355a48c74f6a330f6208564daaa7cbebea9c
+title("Voltage (V) vs Time (s) Circuit D");
 
 % %% Transfer Functions
 % % notice that 10 is 10^1 and 10k is 10^4. |powers| is log(10) .. log(10k)
