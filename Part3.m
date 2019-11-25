8903d95dc3d81c021a77299f87cf15c9161a6baa
peyton-gozon <peyton.gozon@wustl.edu>
2019-11-24 20:18:53 -0600

    123321

--- Part3.m
+++ Part3.m
@@ -61,12 +61,20 @@ ylabel("Voltage (V)");
 title("Circuit C: Voltage (V) vs Time (s)");
 
 %% Circuit D
-AD = [ 1 -1 -1  0  0  0;
-       R  0  0 -1  0  0;
-       0  0  R  0  0 -1;
-       0  0  0  1  0  0;
-       0  0  0  1 -1  0;
-       0  0  0  1 -1 -1;
+% AD = [ 1 -1 -1  0  0  0;
+%        R  0  0 -1  0  0;
+%        0  0  R  0  0 -1;
+%        0  0  0  1  0  0;
+%        0  0  0  1 -1  0;
+%        0  0  0  1 -1 -1;
+%      ];
+
+AD = [  1 -1 -1  0  0  0;
+       -R  0  0  1 -1  0;
+        0  0  R  0  0 -1;
+        0  0  0  1  0  0;
+        0  0  0  0  1  0;
+        0  0 -R  0  1  0;
      ];
 
 % The voltages we're tracking. 
@@ -77,33 +85,23 @@ Vin  = 5 * sin(2 * pi * f1 * timesteps) + sin(2 * pi * f2 * timesteps); % Equati
 
 % These could either be scalar or vector, as the values are tossed between
 % each iteration of the loop. 
-i1 = zeros(1, length(timesteps));
-i2 = zeros(1, length(timesteps));
-i3 = zeros(1, length(timesteps));
 
 for k = 1:length(timesteps)
     % Solve the circuit. 
     x = linsolve(AD, [0, 0, 0, Vin(k), Vc2(k), Vc3(k)]');
     % Manually storing values from x in Ax = b. These stored values are
     % used in the update equations. 
-    i1(k)   = x(1);
-    i2(k)   = x(2); 
-    i3(k)   = x(3);
-    Vin(k)  = x(4);
-    V1(k)   = x(5);
-    Vout(k) = x(6);
-    
-    Vc2(k) = Vin(k) - V1(k);
-    
-    % Update Equations
-    Vc2(k+1) = Vc2(k) + (h / C) * i2(k); % Equation 24
-    Vc3(k+1) = Vc3(k) + (h / C) * i3(k); % Equation 25
+
+    Vc2(k+1) = Vc2(k) + (h / C) * x(2); % Equation 24
+    Vc3(k+1) = Vc3(k) + (h / C) * x(3); % Equation 25
 end
+
+Vout = Vc2 - Vc3;
 
 figure;
 hold on;
 plot(timesteps, Vin);
-plot(timesteps, Vout);
+plot(timesteps, Vout(1:end-1));
 hold off;
 legend("Vin", "Vout", "location", "best");
 
@@ -111,54 +109,44 @@ xlabel("Time (s)");
 ylabel("Voltage (V)");
 title("Circuit D: Voltage (V) vs Time (s)");
 
-% %% Transfer Functions
-% % notice that 10 is 10^1 and 10k is 10^4. |powers| is log(10) .. log(10k)
-% % with steps in logarithmic units. 
-% powers = 1:0.001:4;
-% 
-% Vc1C  = zeros(1, length(timesteps));
-% Vc3C  = zeros(1, length(timesteps));
-% VoutC = zeros(1, length(timesteps));
-% 
-% Vc2D  = zeros(1, length(timesteps));
-% Vc3D  = zeros(1, length(timesteps));
-% VoutD = zeros(1, length(timesteps));
-% 
-% for p = 1:length(powers)
-%     % we're using 10^powers(i) to decode the frequency.
-%     Vin = 5 * sin(2 * pi * 10^powers(p) * [0:length(timesteps)] * h);
-%     for i = 1:length(timesteps)
-%         xC = linsolve(AC, [0, Vin(k), Vc1C(k), Vc3C(k),       0,      0]');
-%         xD = linsolve(AD, [0,      0,       0,  Vin(k), Vc2D(k), Vc3D(k)]');
-%         
-%         i1C(k)   = xC(1);
-%         i3C(k)   = xC(3);
-%         Vc1C(k)  = xC(4) - xC(5);
-%         VoutC(k) = xC(6);
-%         
-%         i1D(k)   = x(1);
-%         i2D(k)   = x(2); 
-%         i3D(k)   = x(3);
-%         Vc2D(k)  = x(4) - x(5);
-%         VoutD(k) = x(6);
-%     
-%     %   Update Equations
-%         Vc1C(k+1) = Vc1C(k) + (h / C) * i1C(k); 
-%         Vc3C(k+1) = Vc3C(k) + (h / C) * i3C(k); 
-%         
-%         Vc2D(k+1) = Vc2D(k) + (h / C) * i2D(k); 
-%         Vc3D(k+1) = Vc3D(k) + (h / C) * i3D(k); 
-%     end
-%     
-%     HC(p) = max(VoutC(k));
-%     HD(p) = max(VoutD(k));
-% end
-% 
-% figure;
-% hold on;
-% plot(powers, log(HC));
-% plot(powers, log(HD));
-% hold off;
-% xlabel("Logarithm of the Frequency in log(Hz)");
-% ylabel("Logarithm of the Transfer Function in log(V)");
-% title("Frequency vs Voltage");
+%% Transfer Functions
+% notice that 10 is 10^1 and 10k is 10^4. |powers| is log(10) .. log(10k)
+% with steps in logarithmic units. 
+powers = 1:0.01:4;
+
+Vc1_C  = zeros(1, length(timesteps));
+Vc3_C  = zeros(1, length(timesteps));
+Vc2_D  = zeros(1, length(timesteps));
+Vc3_D  = zeros(1, length(timesteps));
+
+Vout_C = zeros(1, length(timesteps));
+Vout_D = zeros(1, length(timesteps));
+
+for p = 1:length(powers)
+    % we're using 10^powers(p) to decode the frequency.
+    Vin = 5 * sin(2 * pi * 10^powers(p) * [0:length(timesteps)] * h);
+    
+    % Update function
+    for k = 1:length(timesteps)
+        x_C = linsolve(AC, [0, Vin(k), Vc1_C(k), Vc3_C(k), 0, 0]');
+        x_D = linsolve(AD, [0, 0, 0, Vin(k), Vc2_D(k), Vc3_D(k)]');
+        
+        Vc1_C(k+1) = Vc1_C(k) + (h / C) * x_C(1);
+        Vc3_C(k+1) = Vc3_C(k) + (h / C) * x_C(3);
+        Vc2_D(k+1) = Vc2_D(k) + (h / C) * x_D(2);
+        Vc3_D(k+1) = Vc3_D(k) + (h / C) * x_D(3);
+    end
+    
+    HC(p) = max(Vin - Vc1_C - Vc3_C);
+    HD(p) = max(Vc2_D - Vc3_D);
+end
+
+figure;
+hold on;
+plot(powers, (HC));
+plot(powers, (HD));
+hold off;
+legend("H_C(f)", "H_D(f)");
+xlabel("Logarithm of the Frequency in log(Hz)");
+ylabel("Logarithm of the Transfer Function in log(V)");
+title("Frequency vs Voltage");
